b5b949a424828e29cc317fc65254e137102a9b50
Kshesho <wowplayer808@yahoo.com>
2024-08-08 23:21:59 -0400

    created Game scene; set up Player with PlayerMovement script; added _Notepad txt doc

---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+#region Using Statements
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+/// <summary>
+/// (responsibility of this class)
+/// </summary>
+public class PlayerMovement : MonoBehaviour 
+{
+#region Variables
+
+
+
+#endregion
+#region Base Methods
+
+    void Awake()
+    {
+        
+    }
+
+    void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+#endregion
+
+
+
+}

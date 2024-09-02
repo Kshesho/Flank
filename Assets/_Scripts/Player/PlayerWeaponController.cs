5d90c5793bb2553bab84edb68fc1514934e72604
Brandon Cox <Skelly@SSBSMS-MP.local>
2024-09-02 10:19:41 -0400

    [first mac commit] started creating sword attack animation;

--- Assets/_Scripts/Player/PlayerWeaponController.cs
+++ Assets/_Scripts/Player/PlayerWeaponController.cs
@@ -14,36 +14,44 @@ public class PlayerWeaponController : MonoBehaviour
 
     [SerializeField] GameObject _ninjaStarPref;
     [SerializeField] float _cooldownTime = 0.25f;
-    float _canFireTime = -1;
-    bool CooldownFinished()
+    float _canThrowTime = -1;
+    bool NinjaStarCooldownFinished()
     {
-        if (_canFireTime < Time.time)
+        if (_canThrowTime < Time.time)
             return true;
         return false;
     }
     Vector3 _spawnOffset = new Vector3(0, 0.72f, 0);
 
+    float _swordCooldownTime = 2f;
+    [SerializeField] Animator _swordAnim;
+    float _canSwingTime = -1;
+    bool SwordCooldownFinished()
+    {
+        if (_canSwingTime < Time.time)
+            return true;
+        return false;
+    }
+
 #endregion
 #region Base Methods
 
-    void Awake()
-    {
-        
-    }
-
-    void Start () 
-    {
-		
-	}
-	
 	void Update () 
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (CooldownFinished())
+            if (NinjaStarCooldownFinished())
             {
                 Instantiate(_ninjaStarPref, transform.position + _spawnOffset, Quaternion.identity);
-                _canFireTime = Time.time + _cooldownTime;
+                _canThrowTime = Time.time + _cooldownTime;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (SwordCooldownFinished())
+            {
+                _canSwingTime = Time.time + _swordCooldownTime;
+                _swordAnim.SetTrigger("swing");
             }
         }
 	}
